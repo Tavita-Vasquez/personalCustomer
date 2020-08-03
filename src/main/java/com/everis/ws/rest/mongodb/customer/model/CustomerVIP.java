e196a24a8152d4e5b811e69eef6af0caea24d5cf
Tavita-Vasquez <everis@LIM-HT9WM13.usersad.everis.int>
2020-08-03 17:41:12 -0500

    update microservice customers personal

--- src/main/java/com/everis/ws/rest/mongodb/customer/model/CustomerVIP.java
+++ src/main/java/com/everis/ws/rest/mongodb/customer/model/CustomerVIP.java
@@ -1,8 +1,22 @@
 package com.everis.ws.rest.mongodb.customer.model;
 
-public class CustomerVIP {
+import java.util.List;
 
+import org.springframework.data.annotation.Id;
+
+
+public class CustomerVIP {
+	    @Id
+	    private String id;
+
+	    private String name;
+	    private String lastname;
+	    private String motherLastname;
+	    private String typeproduct;
+	    private String numberBankAccount;
+	    private double maxCreditLimit;
 	
 	
 	
 }
+ 
