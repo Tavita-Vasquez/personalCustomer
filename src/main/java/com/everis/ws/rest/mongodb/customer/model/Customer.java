efd9040dbbf5ea1158ca9760c6a25f187c09a6b7
Tavita-Vasquez <everis@LIM-HT9WM13.usersad.everis.int>
2020-08-04 17:15:30 -0500

    update customer model add annotations

--- src/main/java/com/everis/ws/rest/mongodb/customer/model/Customer.java
+++ src/main/java/com/everis/ws/rest/mongodb/customer/model/Customer.java
@@ -1,6 +1,8 @@
 package com.everis.ws.rest.mongodb.customer.model;
 
 
+import javax.validation.constraints.NotNull;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -24,12 +26,31 @@ public class Customer {
     @Id
     private String id;
 
+	@NotNull(message = "name cant be null")
     private String name;
+
+	@NotNull(message = "lastname cant be null")
     private String lastname;
+
+	@NotNull(message = "motherLastname cant be null")
     private String motherLastname;
+
+	@NotNull(message = "idBankAccount cant be null")
     private String idBankAccount;
+
+	@NotNull(message = "numberBankAccount cant be null")
     private String numberBankAccount;
+
+	@NotNull(message = "maxCreditLimit cant be null")
     private double maxCreditLimit;
     
+	@NotNull(message = "bank cant be null")
+	private String bank;	
+	
+	@NotNull(message = "clienttype cant be null")
+	private String clienttype;
+	
+	@NotNull(message = "clientcode cant be null")
+	private String clientcode;  
    	
 }
